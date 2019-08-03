f366082800468f4a49937cda6c8ccde5f2cba31a
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-03 15:28:05 -0700

    Started adding keydown events

--- src/App.js
+++ src/App.js
@@ -27,6 +27,29 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+
+  function handleKeyDown(event){
+    // console.log(event.key);
+    // console.log(event.key);
+    // if(event.key=="1" || event.key=="2" || event.key=="3" || event.key=="4" || event.key=="5" || event.key=="6" || event.key=="7" || event.key=="8" || event.key=="9" || event.key=="0" || event.key=="."){
+    //   // console.log('number');
+    //   handleNumberClick(event.key);
+    // }
+    // if(event.key=="Escape" || event.key=="Backspace" || event.key=="%"){
+    //   // console.log('number');
+    //   let eventKey = event.key=="Escape" ? "C" : event.key=="Backspace" ? "C" : "%";
+    //   handleSpecialClick(eventKey);
+    // }
+
+    // if(event.key=="+" || event.key=="-" || event.key=="/" || event.key=="*" || event.key=="Enter"){
+    //   // console.log('number');
+      
+    //   handleOperatorClick(event.key);
+    // }
+
+
+  }
+
   function handleSpecialClick(specialString) {
     //If C is pressed once, reset display
     if (specialString == "C" && holdSpecialC == false) {
@@ -309,7 +332,7 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <div className="container" onKeyDown={handleKeyDown}>
       <div className="App">
         <Logo />
         <Display display={display} />
