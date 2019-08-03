dd1182810be80013f636f39c26e9af310c2afba9
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-03 12:24:24 -0700

    Basic operations work

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ import Display from "./components/DisplayComponents/Display";
 
 function App() {
   const [display, setDisplay] = useState("0");
-  const [firstNumber, setFirstNumber] = useState();
+  const [newDisplay, setNewDisplay] = useState(false);
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [holdOperator, setHoldOperator] = useState(null);
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -23,25 +25,127 @@ function App() {
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
   function handleNumberClick(numberString) {
-    if (display.length === 11) {
-      //do nothing
-    } else if (display.includes(".") && numberString === ".") {
-      //do nothing
-    } else if (display === "0" && numberString != ".") {
-      let newNumber = numberString;
-      setDisplay(newNumber);
-    } else if(display==="0" && numberString ==="."){
-      let newNumber = display + numberString;
-      setDisplay(newNumber);
-    }else {
-      let newNumber = display + numberString;
-      setDisplay(newNumber);
+    let newNumber;
+
+    if(newDisplay==true){
+      if (display === "0" && numberString != ".") { 
+        newNumber = numberString;
+        setNewDisplay(false);
+      } else if (display === "0" && numberString === ".") { //If display is only 0, make a new decimal "0."
+        newNumber = display + numberString;
+        setNewDisplay(false);
+      } else {
+        newNumber = numberString; //Start new display string
+        setNewDisplay(false);
+      }
+    }else if (display.length === 11) { //Max display length is 11. This leaves room for a negative at the front
+      return;
+    } else if (display.includes(".") && numberString === ".") { //If display already includes decimal, don't add another
+      return;
+    } else if (display === "0" && numberString != ".") { 
+      newNumber = numberString;
+    } else if (display === "0" && numberString === ".") { //If display is only 0, make a new decimal "0."
+      newNumber = display + numberString;
+    } else {
+      newNumber = display + numberString; //Add to the display string
     }
+    
+    setDisplay(newNumber); //Push to display
   }
 
-  function handleOperatorClick(operator){
-    // console.log(operator);
-    setFirstNumber(display);
+  function handleOperatorClick(operator) {
+
+
+    if((operator==="+" || operator==="-" || operator==="/" || operator==="*" ) && firstNumber === null){
+      setFirstNumber(display);
+      setHoldOperator(operator);
+      setNewDisplay(true);
+    }
+
+    if((operator==="+" || operator==="-" || operator==="/" || operator==="*" ) && firstNumber != null && newDisplay==true){
+      // setFirstNumber(display);
+      setHoldOperator(operator);
+      // setNewDisplay(true);
+    }
+
+    if((operator==="+" || operator==="-" || operator==="/" || operator==="*" || operator==="=" ) && firstNumber != null && newDisplay==false){
+      // setFirstNumber(display);
+      let first = parseInt(firstNumber);
+      let second = parseInt(display);
+      switch(holdOperator) {
+        case "+":
+          let sum = first+second;
+          // console.log(sum.length);
+          if (sum.toString().length>11){
+
+            // Number.parseFloat(sum).toExponential
+            setDisplay(sum.toExponential(6));
+          } else{
+            setDisplay(sum);
+          }
+          
+          
+          setNewDisplay(true);
+          // if(operator != "="){
+            setFirstNumber(sum);
+            setHoldOperator(operator);
+          // }
+          
+          break;
+        case "-":
+            let difference = first-second;
+
+            if(difference.toString().length>11){
+              setDisplay(difference.toExponential(6));
+            } else{
+              setDisplay(difference);
+            }
+            
+            setFirstNumber(difference);
+            setNewDisplay(true);
+            setHoldOperator(operator);
+          // code block
+          break;
+          case "/":
+              let quotient = first/second;
+              if(quotient.toString().length>11){
+                setDisplay(quotient.toExponential(6));
+              } else{
+                setDisplay(quotient);
+              }
+              setFirstNumber(quotient);
+              setNewDisplay(true);
+              setHoldOperator(operator);
+          // code block
+          break;
+          case "*":
+              let product = first*second;
+              if(product.toString().length>11){
+                setDisplay(product.toExponential(6));
+              } else{
+                setDisplay(product);
+              }
+              setFirstNumber(product);
+              setNewDisplay(true);
+              setHoldOperator(operator);
+          // code block
+          break;
+          case "=":
+          // code block
+          break;
+        default:
+          // code block
+      }
+      if(operator=="="){
+        // setHoldOperator(null);
+
+      }
+      
+
+    }
+
+    // console.log(display);
+    
   }
 
   return (
@@ -56,7 +160,7 @@ function App() {
             <Numbers handleNumberClick={handleNumberClick} />
           </div>
 
-          <Operators handleOperatorClick={handleOperatorClick}/>
+          <Operators handleOperatorClick={handleOperatorClick} />
         </div>
       </div>
     </div>
