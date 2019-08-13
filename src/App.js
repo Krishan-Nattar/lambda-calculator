28f3778f2f23611a35c6c595eaff0c3d9b0daa9e
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-13 11:08:52 -0700

    Adding explanatory comments

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ function App() {
 
 
   function handleKeyDown(event){
+    //This will be for future use to be able to type commands on the keyboard and make the calculator function
+
     // console.log(event.key);
     // console.log(event.key);
     // if(event.key=="1" || event.key=="2" || event.key=="3" || event.key=="4" || event.key=="5" || event.key=="6" || event.key=="7" || event.key=="8" || event.key=="9" || event.key=="0" || event.key=="."){
